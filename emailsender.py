9482e003247435ac3f70ee5c80f779bb5824f057
Danish Sharjeel <50940643+dsharjeel@users.noreply.github.com>
2023-03-27 19:03:09 +0500

    Update emailsender.py

--- emailsender.py
+++ emailsender.py
@@ -6,27 +6,20 @@ from email import encoders
 import csv
 
 
-email_addr = 'danishsharjeel3@gmail.com'
-email_pass = 'Aw{VBU&d@Vq&g#1Nu0k'
-email_sndr = 'Danish Sharjeel <danishsharjeel3@gmail.com>'
+email_addr = 'example@gmail.com'
+email_pass = 'password'
+email_sndr = 'Danish Sharjeel <example@gmail.com>'
 
-# email_recr = 'danishsharjeel7@gmail.com'
-# comp_name = 'CoderHogwarts'
-# comp_link = 'coderhogwarts.com'
-
-with open('emails_data.csv') as file:
-    reader = csv.reader(file)
+with open('emails_list.csv') as file:
+    reader = csv.reader(file)         # Containing list of emails
     next(reader)
-    for comp_name, comp_link, email_recr in reader:
-        email_subj = 'Email Subject'  #Enter your email subject here
+    for comp_name, email_recr in reader:
+        email_subj = 'Email Subject'  # Enter your email subject here
 
-        # Email body for data science intrn
-        email_body = f"""<p style="font-size:16px;">Respected Sir/Ma’am,
+        # Email body
+        email_body = f"""<p style="font-size:16px;">Greetings,
          <br/><br/>
-        Most humbly, I am writing this email concerning the available opportunity for a remote internship at {comp_name}.
-        My name is Danish Sharjeel and I am a first-year student of BS Computer Science. I recently started looking for
-        Data Analyst internships and saw your company website {comp_link} listed as a Big Data/AI solution tech company hence I'm
-        emailing for an open opportunity as Remote Data Science Intern.
+         Your Email {comp_name}
         <br/><br/>
         With best regards,<br/>
         Danish Sharjeel
@@ -39,11 +32,11 @@ with open('emails_data.csv') as file:
         send_email["Subject"] = email_subj
         send_email.attach(MIMEText(email_body, 'html'))
 
-        email_attachment = open('Resume.pdf', 'rb')
+        email_attachment = open('image.jpg', 'rb')
         attachment = MIMEBase('application', 'octet-stream')
         attachment.set_payload(email_attachment.read())
         encoders.encode_base64(attachment)
-        attachment.add_header('Content-Disposition', 'attachment; filename=Resume.pdf')
+        attachment.add_header('Content-Disposition', 'attachment; filename=image.jpg')
         send_email.attach(attachment)
 
         server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
